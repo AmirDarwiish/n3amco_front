078be25ade2dae63cdf8f004fc7f39a16557deab
Amir Darwish <amird9051@gmail.com>
2026-06-18 01:10:46 +0300

    Added public products and website settings

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -155,7 +155,6 @@ export default function Footer({ settings = {}, scrollTo }) {
     textAlign: 'center',
     marginTop: 18,
     paddingTop: 18,
-    borderTop: '1px solid rgba(255,255,255,0.06)'
   }}
 >
   <span
