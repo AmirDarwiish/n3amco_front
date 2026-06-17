b7b4694176cdf9b0cb2281414be956eea4a8a311
Amir Darwish <amird9051@gmail.com>
2026-06-17 19:55:29 +0300

    Added public products and website settings

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 // src/config.js
-const API_BASE_URL = 'https://localhost:7068'  // ← غيّر ده بس لما ترفع
+const API_BASE_URL = 'https://n3amco.runasp.net'  // ← غيّر ده بس لما ترفع
 
 export default API_BASE_URL
